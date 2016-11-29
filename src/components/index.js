2b546644a24e567ac91b338aef904f4efc70cbb2
anyunfei <anyunfei@wanda.cn>
2016-11-29 15:22:49 +0800

    add range input component

--- src/components/index.js
+++ src/components/index.js
@@ -2,12 +2,16 @@ import datepicker from './datepicker/index.js'
 import rangepicker from './rangepicker/index.js'
 import checkboxtree from './checkboxtree/index.js'
 import multiselect from './multiselect/index.js'
+import 'angular-touch';
+import './rangeinput/range';
+import './rangeinput/range.less'
 
 
 let componentModule = angular.module('bp.components', [
-  datepicker.name,
-  rangepicker.name,
-  checkboxtree.name,
-  multiselect.name
+	datepicker.name,
+	rangepicker.name,
+	checkboxtree.name,
+	multiselect.name,
+	'ffan.range'
 ]);
 export default componentModule;
