cae33caf3871e67c9ed38452b29caefff9d749a8
dongyu12 <dongyu12@wanda.cn>
2016-12-21 11:17:14 +0800

    增加被选中样式

--- src/components/timeselector/timeselector.js
+++ src/components/timeselector/timeselector.js
@@ -30,21 +30,22 @@ export default class Timeselector extends Component {
    */
   _initDefaultValue() {
     this.showFlag = false;
-    //判断传递的时间列表参数是否合法
+    //判断时间列表参数是否合法
     if(this.timeList && this.timeList instanceof Array){
       this.timeNodes = this.timeList;
     }
     if(!this.timeList || (this.timeList && !(this.timeList instanceof Array))){
       this.timeNodes = ['30分钟','60分钟','3小时','6小时','1天','7天'];
     }
-    //判断传递的初始时间参数是否合法
+    //判断初始时间参数是否合法
     if(this.stateTime && typeof (this.stateTime) == 'string'){
       this.select(this.stateTime);
     }else{
-      this.stateTime = this.timeNodes && this.timeNodes[0];
-      this.select(this.stateTime);
+      this.stateTime = this.timeNodes && this.timeNodes[2];
+      this.index = this.stateTime;
+      this.sendTimer();
     }
-    this.sortList();
+    this.domPos();
   }
 
   /**
@@ -60,31 +61,26 @@ export default class Timeselector extends Component {
    * 格式化时间点
    */
   formatData(timenode){
-    let millisecond = /^(\d+)毫秒$/, second = /^(\d+)秒$/, minute = /^(\d+)分钟$/, hour = /^(\d+)小时$/, day = /^(\d+)天$/, reg = /\d+/g, num = null;
+    let millisecond = /^(\d+)(毫秒|millisecond|ms)$/, second = /^(\d+)(秒|second|s)$/, minute = /^(\d+)(分钟|minute|min|m)$/, hour = /^(\d+)(小时|hour|h)$/, day = /^(\d+)(天|day|days|d)$/, reg = /\d+/g, num = null;
     if(millisecond.test(timenode)){
       num = reg.exec(timenode);
       this.timer = num[0];
-      console.log('毫秒',this.timer);
     }
     if(second.test(timenode)){
       num = reg.exec(timenode);
       this.timer = num[0]*1000;
-      console.log('秒',this.timer);
     }
     if(minute.test(timenode)){
       num = reg.exec(timenode);
       this.timer = num[0]*1000*60;
-      console.log('分钟',this.timer);
     }
     if(hour.test(timenode)){
       num = reg.exec(timenode);
       this.timer = num[0]*1000*60*60;
-      console.log("小时",this.timer);
     }
     if(day.test(timenode)){
       num = reg.exec(timenode);
       this.timer = num[0]*1000*24*60*60;
-      console.log('天',this.timer);
     }
     return this.timer;
   }
@@ -93,8 +89,8 @@ export default class Timeselector extends Component {
    * 选择时间点
    * @param timenode
    */
-  select(timenode){
-    this.index = timenode;
+  select(timenode, e){
+    this.index = this.stateTime =timenode;
     this.showFlag = false;
     this.formatData(timenode);
     this.sendTimer();
@@ -118,9 +114,10 @@ export default class Timeselector extends Component {
    * 排序
    */
   sortList(){
-    let sourceList = this.timeNodes, completeList = [], num = null;
+    let sourceList = this.timeNodes, completeList = [], num = null, cur = {};
     for(let i=0; i<sourceList.length; i++){
       num = this.formatData(sourceList[i]);
+      cur[i] = i;
       completeList.push(num);
     }
     completeList.sort(function(a, b){
@@ -128,6 +125,18 @@ export default class Timeselector extends Component {
     });
     console.log('排序后的',completeList);
     return completeList;
+  }
+
+  /**
+   * 获取位置
+   */
+  domPos(){
+    console.log('window',window);
+    //scroll
+    this.wWidth = document.body.clientWidth;
+    this.btnWidth = document.getElementById('menuBtn').style.left;
+    console.log('window的宽度',this.wWidth);
+    console.log('btn的宽度',this.btnWidth);
   }
 
   /**
